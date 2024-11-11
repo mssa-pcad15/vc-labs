bb9911484e6346de66f9c3b54445e074ac99507c
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-11-11 09:42:00 -0800

    Complete FtoC program that converts Farentheit to Celsius

---
+++ week2/Week2Labs/FtoC/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FtoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fahrenheit = 94;
+            var celsius = 5 * (fahrenheit - 32) / 9f;
+            Console.WriteLine($"{fahrenheit}°F is {celsius:n1}°C.");
+        }
+    }
+}

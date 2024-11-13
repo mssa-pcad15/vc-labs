19a75179c6c3760c87835360922c67b6bd0bd47b
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-11-13 11:15:32 -0800

    completed guided exercise part 2

--- week2/Week2Labs/StudentGradePart2/Program.cs
+++ week2/Week2Labs/StudentGradePart2/Program.cs
@@ -5,31 +5,94 @@
         // initialize variables - graded assignments 
         int currentAssignments = 5;
 
-        int[] sophiaScores = [90, 86, 87, 98, 100];
-        int[] andrewScores = [92, 89, 81, 96, 90];
-        int[] emmaScores = [90, 85, 87, 98, 68];
-        int[] loganScores = [90, 95, 87, 88, 96];
+        int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+        int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+        int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+        int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+        int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+        int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+        int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+        int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 
-        string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
-
-        int[] scores;
-
+        // Student names
+        string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+        
         Console.WriteLine("Student\t\tGrade\n");
 
         foreach (string studentName in studentNames) 
         {
+            int[] scores;
+
             if (studentName == "Sophia") scores = sophiaScores;
             else if (studentName == "Andrew") scores = andrewScores;
             else if (studentName == "Emma") scores = emmaScores;
+            else if (studentName == "Becky") scores = beckyScores;
+            else if (studentName == "Chris") scores = chrisScores; 
+            else if (studentName == "Eric") scores = ericScores;
+            else if (studentName == "Gregor") scores = gregorScores;
             else scores = loganScores;
 
             int scoreSum = 0;
+            int gradedAssignments = 0;
             foreach (int score in scores)
             {
-                scoreSum += score;
+                // increment the assignment counter
+                gradedAssignments += 1;
+                if (gradedAssignments <= currentAssignments)
+                {
+                    scoreSum += score;
+                }
+                else 
+                {
+                    scoreSum += score/10;
+                }
             }
-            decimal studentScore=(decimal) scoreSum / currentAssignments;
-            Console.WriteLine($"{studentName}:\t\t{studentScore}\t?");
+
+            decimal currentStudentGrade = (decimal) scoreSum / currentAssignments;
+
+            string currentStudentLetterGrade = "";
+
+            #region bunch of if else for grade letter
+                if (currentStudentGrade >= 97)
+                    currentStudentLetterGrade = "A+";
+
+                else if (currentStudentGrade >= 93)
+                    currentStudentLetterGrade = "A";
+
+                else if (currentStudentGrade >= 90)
+                    currentStudentLetterGrade = "A-";
+
+                else if (currentStudentGrade >= 87)
+                    currentStudentLetterGrade = "B+";
+
+                else if (currentStudentGrade >= 83)
+                    currentStudentLetterGrade = "B";
+
+                else if (currentStudentGrade >= 80)
+                    currentStudentLetterGrade = "B-";
+
+                else if (currentStudentGrade >= 77)
+                    currentStudentLetterGrade = "C+";
+
+                else if (currentStudentGrade >= 73)
+                    currentStudentLetterGrade = "C";
+
+                else if (currentStudentGrade >= 70)
+                    currentStudentLetterGrade = "C-";
+
+                else if (currentStudentGrade >= 67)
+                    currentStudentLetterGrade = "D+";
+
+                else if (currentStudentGrade >= 63)
+                    currentStudentLetterGrade = "D";
+
+                else if (currentStudentGrade >= 60)
+                    currentStudentLetterGrade = "D-";
+                else
+                    currentStudentLetterGrade = "F";
+            #endregion
+
+            Console.WriteLine($"{studentName}:\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
         }
 
 

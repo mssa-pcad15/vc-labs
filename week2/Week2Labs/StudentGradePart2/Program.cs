562adb9699e29d648d56723b4744bfdaefd4c54e
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-11-13 11:31:41 -0800

    change to collection expression

--- week2/Week2Labs/StudentGradePart2/Program.cs
+++ week2/Week2Labs/StudentGradePart2/Program.cs
@@ -5,17 +5,17 @@
         // initialize variables - graded assignments 
         int currentAssignments = 5;
 
-        int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
-        int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
-        int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
-        int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-        int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
-        int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
-        int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
-        int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
+        int[] sophiaScores = [90, 86, 87, 98, 100, 94, 90];
+        int[] andrewScores = [92, 89, 81, 96, 90, 89];
+        int[] emmaScores = [90, 85, 87, 98, 68, 89, 89, 89];
+        int[] loganScores = [90, 95, 87, 88, 96, 96];
+        int[] beckyScores = [92, 91, 90, 91, 92, 92, 92];
+        int[] chrisScores = [84, 86, 88, 90, 92, 94, 96, 98];
+        int[] ericScores = [80, 90, 100, 80, 90, 100, 80, 90];
+        int[] gregorScores = [91, 91, 91, 91, 91, 91, 91];
 
         // Student names
-        string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+        string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor"];
         
         Console.WriteLine("Student\t\tGrade\n");
 
@@ -94,8 +94,6 @@
 
             Console.WriteLine($"{studentName}:\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
         }
-
-
     
         Console.WriteLine("Press the Enter key to continue");
         Console.ReadLine();

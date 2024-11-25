967373b97024e3908fd1ef3c985314af226a3244
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-11-25 15:15:32 -0800

    finish deal method

---
+++ week4/Fundamentals/LearnTypes/Deck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals.LearnTypes
+{
+    public class Deck
+    {
+        //public static int NumberOfCards = 52;
+        public const int NumberOfCards = 52; //either one works, what is the difference?
+        public bool IsInNewDeckOrder { get; } //flag indicates whether the deck had been shuffled
+        public int RemainingCards => NumberOfCards - currentCard; //RemainingCards is simply NumberOfCards-currentCards
+
+        private Card[] _cards = new Card[NumberOfCards]; // private array of Card
+        private int currentCard = 0;
+        
+        
+
+        public Deck() { // this is constructor, we will create 52 card in Suit/Rank order and set the IsInNewDeckOrder flag to true
+
+            int cardPosition = 0; //this is used to point at each card position
+            //outer loop, using int indexer 
+            for (int i = 1; i <=4 ; i++)
+            {
+                //inner loop, using foreach syntax
+                foreach (CardRank r in Enum.GetValues<CardRank>()) { 
+                    this._cards[cardPosition++] = new Card { Suit = (CardSuit) i , Rank = r };
+                }
+
+            }
+            IsInNewDeckOrder = true;
+        }
+        public Card GetCardByIndex(int index) => _cards[index]; //use a fatarrow to retunr card by index
+
+        public Card Deal() => GetCardByIndex(currentCard++); //we already have a GetCardByIndex, let's reuse it.
+
+
+    }
+}
--- week4/FundamentalsTest/PlayingCardsTest.cs
+++ week4/FundamentalsTest/PlayingCardsTest.cs
@@ -15,7 +15,7 @@ namespace FundamentalsTest
         {
             //Arrange
             Card aceOfSpade = new Card() { Rank = CardRank.Ace, Suit = CardSuit.Spade };
-            string expected = "♠11";
+            string expected = "♠1";
             //Act
             string actual = aceOfSpade.ToString();
             //Assert
@@ -25,13 +25,37 @@ namespace FundamentalsTest
         [TestMethod] //1
         public void ADeckShouldAlwaysHave52Cards()
         {
+            //add a new public class in Fundamentals project called Deck.
+            // The class Deck must have a static int member NumberOfCards that is assigned 52
+
             //Arrange
+            int actual = Deck.NumberOfCards;
+            int expected = 52;
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod] //2
         public void NewDeckOfCardWillHaveCardsInSuitToRankOrder() //Spade A-13, Heart A-13
-        {
+        {   // declare private Card[] that has capacity of NumberOfCards
+
+            // in the constructor of Deck class, use 2 loops, where the outer loop enumerate Suite
+            // and inner loop enumerate Rank to popuplate the private array
+
+            // lets add a public readonly bool property called IsInNewDeckOrder
+
+            // Add a method called GetCard that returns a Card and accept an int as index of array
             //Arrange
+            Deck newDeck = new Deck(); //instantiate a new Deck object, which calls the constructor
+
+            Card actual =  newDeck.GetCardByIndex(0); //Get the first card in the array. !! leaky implementation
+            Card expected = new Card { Suit = CardSuit.Spade, Rank = CardRank.Ace }; //first card is ace of spade
+            Assert.AreEqual(actual, expected); // two variable of struct type is compared by value, so if two cards of the same suit and rank, they are equal.
+
+            Card actualLast = newDeck.GetCardByIndex(51); //Get the last card in the array. !! leaky implementation
+            Card expectedLast = new Card { Suit = CardSuit.Diamond, Rank = CardRank.King }; //last card is king of diamond
+            Assert.AreEqual(actualLast, expectedLast);
+
         }
         [TestMethod] //5
         public void ADeckCanBeShuffled()
@@ -42,10 +66,31 @@ namespace FundamentalsTest
         [TestMethod] //3
         public void DealACardShouldReturnNextCardInTheDeck()//Should return the next card in Deck
         {
+            // initialize a private field to serve as counter to number of cards dealt
+            // write an instance method that deals the next card
+            // provide a method called Deal() that returns a card according to the counter
+            // also provide a get property that indicates how many cards left in the deck
+
             //Arrange
+            Deck deckA = new Deck();
+            //Act
+            _ = deckA.Deal(); // _ means discard
+            _ = deckA.Deal();
+            Card thirdCard = deckA.Deal(); //thats the third card
+            //Assert
+            Card expected = new Card { Rank = CardRank.Three, Suit = CardSuit.Spade };
+            Assert.AreEqual(expected, thirdCard);
+            Assert.AreEqual(deckA.RemainingCards, 49);
+        }
+        [TestMethod()] //3B
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void CallDealBeyondLastCardShouldThrowIndexOutOfRangeException()//Should return the next card in Deck
+        {
+            var deckA = new Deck();
+            for (int i = 0; i < 53; i++) { _ = deckA.Deal(); }
         }
 
-        [TestMethod] //4
+            [TestMethod] //4
         public void PrintsCardsInDeck() //
         {
             //Arrange

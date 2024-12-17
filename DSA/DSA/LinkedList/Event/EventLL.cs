0554bfdc4394f508103bec897bb09e7baad6c202
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-12-17 14:58:42 -0800

    partial starting point for Event Driven Linked List

---
+++ DSA/DSA/LinkedList/Event/EventLL.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.LinkedList.Event
+{
+    public class EventLL<T>: IEnumerable<T>,IEnumerator<T>
+    {
+        public Node<T>? First = null;
+        public Node<T>? Last = null;
+        public Node<T>? Current = null;
+        private int _currentPointer = -1;
+        object IEnumerator.Current => this.Current;
+
+        T IEnumerator<T>.Current => this.Current.Value;
+
+        internal event EventHandler<NodeEventsArgs<T>>? OnCommand;
+
+        public void Dispose()
+        {
+            ;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (First == null || Last == null) return false;
+            if (_currentPointer== -1 )
+            {
+                _currentPointer = 0;
+                return true;
+            }
+            _currentPointer++;
+            return NodeExistsByIndex(_currentPointer);
+        }
+
+        private bool NodeExistsByIndex(int currentPointer)
+        {
+           var arg = new NodeEventsArgs<T>() { TypeOfCommand = NodeCommandType.NodeSearchByIndex,
+           new Node<object>(new object()) {  }
+           }
+        }
+
+        public void Reset()
+        {
+            _currentPointer = -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Node<T> 
+    {
+        internal int Index = -1;
+
+        public T Value;
+        public Node<T>? Next;
+
+        public Node()
+        {
+            Value = default;
+        }
+        public Node(T input)
+        {
+            Value = input;
+        }
+        public Node(T input, EventLL<T> owner) : this(input) 
+        {
+            owner.OnCommand += this.HandleEvent!;
+        }
+        private void HandleEvent(object sender, NodeEventsArgs<T> arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DSA/DSA/LinkedList/Event/NodeEventsArgs.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace DSA.LinkedList.Event
+{
+    internal class NodeEventsArgs<T>
+    {
+        internal NodeCommandType TypeOfCommand;
+        internal Node<T> Target;
+
+        internal int SearchByIndex;
+        internal Node<T>? searchByIndexResult;
+
+        internal T SearchByNodeValue;
+        internal List<Node<T>>? searchByValueResult;
+    }
+    internal enum NodeCommandType
+    { 
+        NodeAdded,
+        NodeRemoved,
+        NodeSearchByIndex,
+        NodeSearchByValue
+    }
+}

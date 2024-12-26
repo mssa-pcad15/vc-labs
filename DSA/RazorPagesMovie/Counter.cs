4d716c4c19404437cf0d29cb8d1df92508153a20
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-12-26 11:58:49 -0800

    migrated configurations to AppSettings.json and Secrets.json

--- DSA/RazorPagesMovie/Counter.cs
+++ DSA/RazorPagesMovie/Counter.cs
@@ -30,37 +30,35 @@ namespace RazorPagesMovie
     //Step 1: Create another inherit class from Counter
     public class SingletonDistributedCounter : Counter {
         private readonly TableClient table;
+        private readonly IConfiguration config;
 
-        public SingletonDistributedCounter(TableClient table) //this will be used as singleton, so ctor only called once.
+        public SingletonDistributedCounter(TableClient table,IConfiguration config) //this will be used as singleton, so ctor only called once.
             // use a private readonly field to keeep reference.
         {
             this.table = table;
+            this.config = config;
             //Step2: move constructor logic to Getter and Setter of Count Property
             // marking Counter.Count prop as virtual see line 9
-
         }
-
         public override int Count
         {
             get { //step 3: on Get of Count Prop, use TableClient table (see line32) to retreive the Count from Azure Table
                 try //put the GetEntity in Try Block
                 {
-                    Response<CounterRow> result = table.GetEntity<CounterRow>("0", "chinjila");
+                    Response<CounterRow> result = table.GetEntity<CounterRow>(config["PartitionKey"], config["RowKey"]);
                     return result.Value.Count;
                 }
                 catch (RequestFailedException rfe) { //GetEntity Failed, is it because the row is missing?
                     if (rfe.ErrorCode == "404") { //
-                        CounterRow newRow = new CounterRow { PartitionKey = "0", RowKey = "chinjila", Count = 0 };
+                        CounterRow newRow = new CounterRow { PartitionKey = config["PartitionKey"], RowKey = config["RowKey"]!, Count = 0 };
                         table.AddEntity<CounterRow>(newRow);
                     }
                     return 0;
                 }
-
-                
             }
             set { //step 4: on Set of Count Prop, update Azure Table Record
 
-                CounterRow updatedRow = new CounterRow { PartitionKey = "0", RowKey = "chinjila", Count = value };
+                CounterRow updatedRow = new CounterRow { PartitionKey = config["PartitionKey"], RowKey = config["RowKey"]!, Count = value };
                 table.UpsertEntity<CounterRow>(updatedRow, TableUpdateMode.Replace);
             }
         

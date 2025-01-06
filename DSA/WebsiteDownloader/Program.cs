95122607eff58bf10e909c47bbbef07712fd4026
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2025-01-06 15:36:36 -0800

    Add queue management

--- DSA/WebsiteDownloader/Program.cs
+++ DSA/WebsiteDownloader/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Collections.Concurrent;
+using WebsiteDownloader;
+
+Uri[] sitesUrl = [
+                new Uri("https://google.com"),
+                new Uri("https://msn.com"),
+                new Uri("https://abc.com"),
+                new Uri("https://wikipedia.com"),
+                new Uri("https://cnbc.com"),
+                new Uri("https://bloomberg.com"),
+                new Uri("https://weather.com"),
+];
+
+ConcurrentQueue<Downloader> downloadersQueue = new();
+Parallel.ForEach(sitesUrl, site=> downloadersQueue.Enqueue(new Downloader(site))); // multi threaded
+//parallel forEach
+
+List<Task<int>> downloadTasks = new();
+while (!downloadersQueue.IsEmpty) {
+    if (downloadersQueue.TryDequeue(out Downloader downloader))
+    { downloadTasks.Add(downloader.DownloadAsync()); }
+}
+
+await Task.WhenAll(downloadTasks);
+int totalBytes = downloadTasks.Sum(d=>d.Result);
+Console.WriteLine($"All sites downloaded, {totalBytes} bytes written.");

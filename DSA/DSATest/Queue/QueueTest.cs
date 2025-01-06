1f0391a9d0ab9a77d57286d0da5e5c73e65196fd
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2025-01-06 11:19:39 -0800

    Added Async Queue Exercise

---
+++ DSA/DSATest/Queue/QueueTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSATest.Queue
+{
+    [TestClass]
+    public class QueueTest
+    {
+        [TestMethod]
+        public void EnqueueTenThings() { 
+            Queue<int> queue= new Queue<int>();
+            for (int i = 0; i < 10; i++) { 
+            queue.Enqueue(i);
+            }
+            Assert.AreEqual(10, queue.Count); //should be 10 in queue
+            Assert.AreEqual(0,queue.Peek()); // peek return the first item without removing it
+            Assert.AreEqual(10, queue.Count); //count is still ten
+            Assert.AreEqual(0, queue.Dequeue()); //dequeue remove the head of the queue and return it
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(7, queue.Count); // now there are 7 things in the queue
+        }
+        [TestMethod]
+        public async Task AsyncEnqueuLotsOfItemsAsync()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            Task t1 = InsertItemAsync(queue, 10);
+            Task t2 = InsertItemAsync(queue, 10);
+            Task t3 = InsertItemAsync(queue, 10);
+            Task t4 = InsertItemAsync(queue, 10);
+            Task t5 = InsertItemAsync(queue, 10);// this method is asynchronous
+
+            //await keyword for a task cause it to block
+          
+            Assert.AreEqual(50,queue.Count);
+        }
+
+        [TestMethod]
+        public async Task ConcurrentEnqueuLotsOfItemsAsync()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            Parallel.For(0, 5, loopCounter => InsertItemAsync(queue, 10)); 
+
+            Assert.AreEqual(50, queue.Count);
+        }
+        private async Task InsertItemAsync(Queue<int> queue, int numberOfItem) {
+            for (int i = 0; i < numberOfItem; i++)
+            { 
+                queue.Enqueue(i);
+                await Task.Delay(10);
+                
+            }
+        }
+    }
+}
--- DSA/RazorPagesMovie/Program.cs
+++ DSA/RazorPagesMovie/Program.cs
@@ -5,49 +5,55 @@ using RazorPagesMovie;
 using Microsoft.Extensions.Configuration;
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-//read the ConnectionString and TableName from built-in ConfigurationService
-builder.Services.AddSingleton(
-    typeof(TableClient),
-    new TableClient(builder.Configuration["ConnectionString"], builder.Configuration["TableName"]) // this works!!
-    );
-
-//builder.Services.AddTransient(typeof(Counter), typeof(DistributedCounter));
-builder.Services.AddSingleton(typeof(Counter), typeof(SingletonDistributedCounter));
-//step 5 : comment out line 20 and replace with AddSingleton
-
-//AddSingleton is used for "GLOBAL" "STATEFUL" service
-//lottery history
-//leaderboard
-//cache service
-//step 2, add Menu to DI Container
-//line 15 is the same as line 8, just use a shorter version of syntax
-builder.Services.AddSingleton<Menu>(new Menu { ProteinOfTheDay = "Chicken", SoupOfTheDay = "Tomato" });
-
-// singleton:
-// con: scalability - because the same instance MUST be available -- how do we scale across machines?
-WebApplication ? app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+
+        //read the ConnectionString and TableName from built-in ConfigurationService
+        builder.Services.AddSingleton(
+            typeof(TableClient),
+            new TableClient(builder.Configuration["ConnectionString"], builder.Configuration["TableName"]) // this works!!
+            );
+
+       //builder.Services.AddTransient(typeof(Counter), typeof(DistributedCounter));
+        builder.Services.AddSingleton(typeof(Counter), typeof(SingletonDistributedCounter));
+        //step 5 : comment out line 20 and replace with AddSingleton
+
+        //AddSingleton is used for "GLOBAL" "STATEFUL" service
+        //lottery history
+        //leaderboard
+        //cache service
+        //step 2, add Menu to DI Container
+        //line 15 is the same as line 8, just use a shorter version of syntax
+        builder.Services.AddSingleton(new Menu { ProteinOfTheDay = "Chicken", SoupOfTheDay = "Tomato" });
+
+        // singleton:
+        // con: scalability - because the same instance MUST be available -- how do we scale across machines?
+        WebApplication? app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
